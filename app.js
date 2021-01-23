4a24c0791eddfde3a4568cea57c31d0a2bc1ae71
rabbihossain01 <golamrabbi.dev@gmail.com>
2021-01-23 18:33:52 +0600

    Project done

---
+++ app.js
@@ -0,0 +1,22 @@
+const result = document.getElementById('result');
+const submit = document.getElementById('submit');
+
+function binary(e) {
+  e.preventDefault();
+
+  const number = document.getElementById('number').value;
+
+  if (number === '') {
+    alert('Please enter a number');
+  } else if (number < 0) {
+    alert('Please enter your positive number')
+  } else {
+    result.style.visibility = 'visible';
+  }
+
+  // converting
+  binaryNumber = Number(number).toString(2);
+
+  result.innerHTML = binaryNumber;
+}
+submit.addEventListener('click', binary);
